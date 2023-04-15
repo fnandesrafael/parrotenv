1f9dc85f0b8b661b3b2f34fbd5fba8fae02f78f8
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-15 10:52:15 -0300

    refact: refact main function logic to call other modules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import inquirer from 'inquirer';
+import { exec } from 'child_process';
 import stripAnswers from './helpers/stripAnswers.js';
 import questions from './data/questions.js';
 import { askAboutNodeProject, configureEditor } from './modules/index.js';
+
+const initSetup = async (answers) => {
+  await configureEditor(answers.ide);
+};
 
 const main = async () => {
   const answers = await inquirer.prompt(questions);
   const stripedAnswers = stripAnswers(answers);
 
   const hasInit = await askAboutNodeProject(stripedAnswers.npm_init);
-  await configureEditor(stripedAnswers.ide, hasInit);
+
+  if (hasInit) {
+    initSetup(stripedAnswers);
+  } else {
+    exec('npm init -y', { cwd: './mock' });
+    initSetup(stripedAnswers);
+  }
 };
 
 export default main;

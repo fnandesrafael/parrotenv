57055454fd07da9fef34f845053b431f1137f88e
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-16 19:36:11 -0300

    feat: add new questions on inquirer prompt

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -4,7 +4,32 @@ const questions = [
   {
     name: 'npm_init',
     type: 'list',
-    message: `🤔 Have you already initialized a node project with the ${chalk.greenBright('npm init -y')} command?`,
+    message: `🤔 Have you already initialized a ${chalk.greenBright('node')} project?`,
+    choices: ['Yes', 'No'],
+  },
+  {
+    name: 'build_tool',
+    type: 'list',
+    message: '🛠️  Which build tool have you used to create this project?',
+    choices: [
+      chalk.yellow('Vite'),
+      'Another',
+    ],
+  },
+  {
+    name: 'project_type',
+    type: 'list',
+    message: `🍧 What type of ${chalk.yellow('JavaScript')} project are you developing?`,
+    choices: [
+      chalk.yellow('JavaScript'),
+      `React w/ ${chalk.yellow('JavaScript')}`,
+      chalk.blue('TypeScript'),
+    ],
+  },
+  {
+    name: 'linting',
+    type: 'list',
+    message: `🧹 Do you want to configure ${chalk.magentaBright('ESLint')} and ${chalk.magentaBright('Prettier')} to lint and format your code?`,
     choices: ['Yes', 'No'],
   },
   {
@@ -15,6 +40,12 @@ const questions = [
       chalk.blueBright('VS Code'),
       'Another',
     ],
+  },
+  {
+    name: 'stylelint',
+    type: 'list',
+    message: `🎨 Perchance your'e planning on writing any ${chalk.blue('CSS')}, would you like to set up a standard linting configuration with ${chalk.magentaBright('Stylelint')}?`,
+    choices: ['Yes', 'No'],
   },
 ];
 

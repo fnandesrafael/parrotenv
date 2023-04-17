1fde71d34cefa9610147e31cd091d8f242dadeb9
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-17 18:23:56 -0300

    update: minimal modifications on directories

--- src/scripts/writeEditorSettings.ts
+++ src/scripts/writeEditorSettings.ts
@@ -3,7 +3,7 @@ import fs from 'fs-extra';
 
 const writeEditorSettings = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
-  // const filePath = path.resolve(projectRoot, './node_modules/parrotenv/public/.vscode');
+  // const filePath = path.resolve(projectRoot, './node_modules/parrotenv/templates/.vscode');
   const filePath = path.resolve(projectRoot, '../templates/.vscode');
 
   await fs.copy(filePath, path.resolve(projectRoot, '.vscode'));
--- src/scripts/writeViteConfig.ts
+++ src/scripts/writeViteConfig.ts
@@ -3,8 +3,12 @@ import fs from 'fs-extra';
 
 const writeViteConfig = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
-  // const filePath = path.resolve(projectRoot, './node_modules/parrotenv/public/vite.config.js');
-  const filePath = path.resolve(projectRoot, '../templates/vite.config.js');
+  /**
+   * const filePath = path.resolve(
+   *  projectRoot, './node_modules/parrotenv/templates/react/vite.config.js'
+   * );
+   */
+  const filePath = path.resolve(projectRoot, '../templates/react/vite.config.js');
 
   await fs.copy(filePath, path.resolve(projectRoot, 'vite.config.js'));
 };

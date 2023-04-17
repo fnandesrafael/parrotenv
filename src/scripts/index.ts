2b76533d18aecc2188037df59f4dceedba48810d
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-17 10:21:59 -0300

    feat: add setup of vite.config.js file

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,9 @@
 import writeEditorConfig from './writeEditorConfig.js';
 import writeSettingsJson from './writeSettingsJson.js';
+import writeViteConfig from './writeViteConfig.js';
 
 export {
   writeEditorConfig,
   writeSettingsJson,
+  writeViteConfig,
 };

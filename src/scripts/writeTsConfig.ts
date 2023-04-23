d0eb1351f426c59029678ec3437a518f4409274f
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-23 11:38:48 -0300

    refact: new refactor on templates directories

--- src/scripts/writeTsConfig.ts
+++ src/scripts/writeTsConfig.ts
@@ -5,10 +5,10 @@ const writeTsConfig = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
   /**
    * const filePath = path.resolve(
-   *  projectRoot, './node_modules/parrotenv/templates/typescript/tsconfig.json'
+   *  projectRoot, './node_modules/parrotenv/templates/linting/typescript/tsconfig.json'
    * );
    */
-  const filePath = path.resolve(projectRoot, '../templates/typescript/tsconfig.json');
+  const filePath = path.resolve(projectRoot, '../templates/linting/typescript/tsconfig.json');
 
   await fs.copy(filePath, path.resolve(projectRoot, 'tsconfig.json'));
 };

e78e999db49f3a1cd33dce6556a2d2d825acf302
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-24 18:57:55 -0300

    fix: writeTsConfig.ts script paths

--- src/scripts/writeTsConfig.ts
+++ src/scripts/writeTsConfig.ts
@@ -5,10 +5,10 @@ const writeTsConfig = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
   /**
    * const filePath = path.resolve(
-   *  projectRoot, './node_modules/parrotenv/templates/linting/typescript/tsconfig.json'
+   *  projectRoot, './node_modules/parrotenv/templates/linters/ts_tsconfig.json'
    * );
    */
-  const filePath = path.resolve(projectRoot, '../templates/linting/typescript/tsconfig.json');
+  const filePath = path.resolve(projectRoot, '../templates/linters/ts_tsconfig.json');
 
   await fs.copy(filePath, path.resolve(projectRoot, 'tsconfig.json'));
 };

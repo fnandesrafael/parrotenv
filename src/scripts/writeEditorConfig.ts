fd9ade0b45b26df16133db520f16b703306db57b
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-24 19:09:58 -0300

    fix: writeEditorConfig.ts script paths

--- src/scripts/writeEditorConfig.ts
+++ src/scripts/writeEditorConfig.ts
@@ -5,10 +5,10 @@ const writeEditorConfig = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
   /**
    * const filePath = path.resolve(
-   *   projectRoot, './node_modules/parrotenv/templates/editor/.editorconfig'
+   *   projectRoot, './node_modules/parrotenv/templates/editors/vscode_.editorconfig'
    * );
    */
-  const filePath = path.resolve(projectRoot, '../templates/editor/.editorconfig');
+  const filePath = path.resolve(projectRoot, '../templates/editors/vscode_.editorconfig');
 
   await fs.copy(filePath, path.resolve(projectRoot, '.editorconfig'));
 };

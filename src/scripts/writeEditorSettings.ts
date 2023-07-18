26c92003d47367c76a027c541743c84e61cd12d1
fnandesrafael <fnandesrafael@pm.me>
2023-07-18 08:29:24 -0300

    refact: setupEditor module

--- src/scripts/writeEditorSettings.ts
+++ src/scripts/writeEditorSettings.ts
@@ -5,10 +5,10 @@ const writeEditorSettings = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
   /**
    * const filePath = path.resolve(
-   *   projectRoot, './node_modules/parrotenv/templates/editors/.vscode'
+   *   projectRoot, './node_modules/parrotenv/templates/editors/vscode/.vscode'
    * );
    */
-  const filePath = path.resolve(projectRoot, '../templates/editors/.vscode');
+  const filePath = path.resolve(projectRoot, '../templates/editors/vscode/.vscode');
 
   await fs.copy(filePath, path.resolve(projectRoot, '.vscode'));
 };

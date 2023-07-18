9ab4a29797699983282174bed5c24754409d21a8
fnandesrafael <fnandesrafael@pm.me>
2023-07-18 08:27:18 -0300

    update: writeTsConfig script
    and refact writeViteConfig script

--- src/scripts/writeTsConfig.ts
+++ src/scripts/writeTsConfig.ts
@@ -5,10 +5,10 @@ const writeTsConfig = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
   /**
    * const filePath = path.resolve(
-   *  projectRoot, './node_modules/parrotenv/templates/linters/ts_tsconfig.json'
+   *  projectRoot, './node_modules/parrotenv/templates/settings//typescript/tsconfig.json'
    * );
    */
-  const filePath = path.resolve(projectRoot, '../templates/linters/ts_tsconfig.json');
+  const filePath = path.resolve(projectRoot, '../templates/settings/typescript/tsconfig.json');
 
   await fs.copy(filePath, path.resolve(projectRoot, 'tsconfig.json'));
 };
--- src/scripts/writeViteConfig.ts
+++ src/scripts/writeViteConfig.ts
@@ -5,10 +5,10 @@ const writeViteConfig = async () => {
   const projectRoot = path.resolve(process.cwd(), 'mock');
   /**
    * const filePath = path.resolve(
-   *  projectRoot, './node_modules/parrotenv/templates/tools/vite/vite.config.js'
+   *  projectRoot, './node_modules/parrotenv/templates/settings/vite/vite.config.js'
    * );
    */
-  const filePath = path.resolve(projectRoot, '../templates/tools/vite/vite.config.js');
+  const filePath = path.resolve(projectRoot, '../templates/settings/vite/vite.config.js');
 
   await fs.copy(filePath, path.resolve(projectRoot, 'vite.config.js'));
 };
